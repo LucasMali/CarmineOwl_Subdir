9da49ed27a10d0c1c3ef3ae31532150493f7c94e
Lucas M <lucascube@gmail.com>
2020-02-29 18:20:48 -0700

    Adding in the view and travis

---
+++ Block/Main.php
@@ -0,0 +1,12 @@
+<?php
+namespace CarmineOwl\Subdir\Block;
+
+/**
+ * Class Main
+ */
+class Main extends \Magento\Framework\View\Element\Template
+{
+    public function _prepareLayout()
+    {
+    }
+}
